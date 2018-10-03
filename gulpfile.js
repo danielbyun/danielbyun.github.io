11960004b88001fc0071caf3bbf4319a847bf198
Daniel Byun <dbyun92@gmail.com>
2018-10-03 22:30:23 +0900

    update

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+let gulp = require("gulp");
+
+// hello example
+gulp.task("sayHello", function() {
+    console.log("hello");
+});
+
+// gulp.task("minify", function(){
+//    return gulp.src("app/css/reset.css")
+//        .pipe("")
+//        .pipe(gup.dest("disp/css/reset.css"))
+// });
