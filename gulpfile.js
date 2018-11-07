799808d54ef715429f9486696cc7e8d34c835daf
Daniel Byun <dbyun92@gmail.com>
2018-11-07 11:53:38 +0900

    reset node_modules
    manually added auto-prefixer for now

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 let gulp = require("gulp");
+let autoprefixer = require("gulp-autoprefixer");
 
 // hello example
 gulp.task("sayHello", function() {
     console.log("hello");
 });
 
+gulp.task("styles", function() {
+    gulp.src("css/**/*.scss")
+        .pipe(autoprefixer())
+        .pipe(gulp.dest("dist"))
+});
+
 // gulp.task("minify", function(){
 //    return gulp.src("app/css/reset.css")
 //        .pipe("")
-//        .pipe(gup.dest("disp/css/reset.css"))
+//        .pipe(gulp.dest("dist/css/reset.css"))
 // });
-
